b050ea5fea81e20d402b515db56c9c2a391b60b9
MdAbuNafeeIbnaZahid <1305023.maniz@ugrad.cse.buet.ac.bd>
2018-08-03 22:45:40 +0600

    pasted all codes of train_utils.py to train.py. Renamed train_utils.py to unused_train_utils.py

--- speech-recognition-neural-network/train.py
+++ speech-recognition-neural-network/train.py
@@ -9,6 +9,21 @@ from keras.layers import (BatchNormalization, Conv1D, Dense, Input,
 
 
 
+###########  import statements for train_utils.py #############
+from data_generator import AudioGenerator
+import _pickle as pickle
+
+from keras import backend as K
+from keras.models import Model
+from keras.layers import (Input, Lambda, BatchNormalization)
+from keras.optimizers import SGD, RMSprop
+from keras.callbacks import ModelCheckpoint   
+import os
+#####################################################
+
+
+
+
 
 
 from data_generator import vis_train_features
@@ -30,9 +45,9 @@ set_session(tf.Session(config=config))
 #%load_ext autoreload
 #%autoreload 2
 # import NN architectures for speech recognition
-# from sample_models import *
+# from sample_models import *  # I have pasted code of sample_models in this file. So no need to import this
 # import function for training acoustic model
-from train_utils import train_model
+# from train_utils import train_model  # I have pasted code of train_utils in this file. So no need to import this
 
 import numpy as np
 from data_generator import AudioGenerator
@@ -44,7 +59,7 @@ from IPython.display import Audio
 
 
 
-######################  All codes / model defined in sample_models.py ################
+######################  All codes / model defined in sample_models.py start here ################
 
 def simple_rnn_model(input_dim, output_dim=29):
     """ Build a recurrent network for speech 
@@ -229,8 +244,96 @@ def final_model(input_dim, filters, kernel_size, conv_stride,
     print(model.summary())
     return model
 
-
 ##################################### code / model defined in sample_models.py ends here ##############################
+
+
+
+
+
+########################## all codes of train_utils.py starts here #########################
+
+
+def ctc_lambda_func(args):
+    y_pred, labels, input_length, label_length = args
+    #print("y_pred.shape = " + str(y_pred.shape))
+    #print("labels.shape = " + str(labels.shape))
+    #print("input_length.shape = " + str(input_length.shape))
+    #print("label_length.shape = " + str(label_length.shape))
+    return K.ctc_batch_cost(labels, y_pred, input_length, label_length)  # input_length= seq length of each item in y_pred
+	# label_length is the seq length of each item in labels
+
+def add_ctc_loss(input_to_softmax):
+    the_labels = Input(name='the_labels', shape=(None,), dtype='float32')
+    input_lengths = Input(name='input_length', shape=(1,), dtype='int64')
+    label_lengths = Input(name='label_length', shape=(1,), dtype='int64')
+    output_lengths = Lambda(input_to_softmax.output_length)(input_lengths)
+    # output_length = BatchNormalization()(input_lengths)
+    # CTC loss is implemented in a lambda layer
+
+    loss_out = Lambda(ctc_lambda_func, output_shape=(1,), name='ctc')(
+        [input_to_softmax.output, the_labels, output_lengths, label_lengths])
+    model = Model(
+        inputs=[input_to_softmax.input, the_labels, input_lengths, label_lengths], 
+        outputs=loss_out)
+    return model
+
+def train_model(input_to_softmax, 
+                pickle_path,
+                save_model_path,
+                train_json='train_corpus.json',
+                valid_json='valid_corpus.json',
+                minibatch_size=20,
+                spectrogram=True,
+                mfcc_dim=13,
+                optimizer=SGD(lr=0.02, decay=1e-6, momentum=0.9, nesterov=True, clipnorm=5),
+                epochs=20,
+                verbose=1,
+                sort_by_duration=False,
+                max_duration=10.0):
+    
+    # create a class instance for obtaining batches of data
+    audio_gen = AudioGenerator(minibatch_size=minibatch_size, 
+        spectrogram=spectrogram, mfcc_dim=mfcc_dim, max_duration=max_duration,
+        sort_by_duration=sort_by_duration)
+    # add the training data to the generator
+    audio_gen.load_train_data(train_json)
+    audio_gen.load_validation_data(valid_json)
+    # calculate steps_per_epoch
+    num_train_examples=len(audio_gen.train_audio_paths)
+    steps_per_epoch = num_train_examples//minibatch_size
+    # calculate validation_steps
+    num_valid_samples = len(audio_gen.valid_audio_paths) 
+    validation_steps = num_valid_samples//minibatch_size
+    
+    # add CTC loss to the NN specified in input_to_softmax
+    model = add_ctc_loss(input_to_softmax)
+
+    # CTC loss is implemented elsewhere, so use a dummy lambda function for the loss
+    model.compile(loss={'ctc': lambda y_true, y_pred: y_pred}, optimizer=optimizer)
+
+    # make results/ directory, if necessary
+    if not os.path.exists('results'):
+        os.makedirs('results')
+
+    # add checkpointer
+    checkpointer = ModelCheckpoint(filepath='results/'+save_model_path, verbose=0)
+
+    # train the model
+    hist = model.fit_generator(generator=audio_gen.next_train(), steps_per_epoch=steps_per_epoch,
+        epochs=epochs, validation_data=audio_gen.next_valid(), validation_steps=validation_steps,
+        callbacks=[checkpointer], verbose=verbose)
+
+    # save model loss
+    with open('results/'+pickle_path, 'wb') as f:
+        pickle.dump(hist.history, f)
+
+################################ all codes of train_utils.py ends here #######################################
+
+
+
+
+
+
 
 
 
